0e4035c93d900e9dadb2aa8d27734ec868313e3b
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-04-03 17:46:43 -0700

    changed files

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+      <Ingredients_In_Season />
+      <Recipes />
+    </div>
+  )
+}
